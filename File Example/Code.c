d098693b98e68c97a23dca2516071c11c6f38e87
Shiro <iHaiDeeZi@gmail.com>
2020-01-06 19:29:18 +0300

    Update Code.c

--- File Example/Code.c
+++ File Example/Code.c
@@ -1,4 +1,4 @@
-	/* 
+/* 
  *  Author: ShiroKlein
  *  Description: KiwiDog Task
  */
@@ -9,10 +9,18 @@
 //#include <sys/stat.h>
 #include <errno.h>	
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <limits.h>
 void copying_file(char *filecopied,char *argm);
-int isFile(const char *ff_name);
-void openandclosedir(char *argn,char* filepath);
+void openandclosedir(char filepath[]);
+//void openandclosedir2(char *argn,char* filepath);
 int copying_folders(char *foldercopied, char *path);
+int isFile(char *checkpath,const char *ff_name);
+const char *  FolderPathcorrector(char *sher);
+
+
+
 
 int main(int argc, char* argv[] ){
 	
@@ -20,36 +28,36 @@ int main(int argc, char* argv[] ){
 
 	//Messages to be printed
 
-		openandclosedir(originalpath, originalpath);
+		openandclosedir(originalpath);
 
 	return 0;
 
 
 }
 // Open Parent Dir 
-void openandclosedir(char *argn,char* filepath)
+void openandclosedir(char filepath[])
 {
-	FILE *Shiro,*Tut1;
+	//FILE *Shiro,*Tut1;
 	DIR* folder;
 	struct dirent *shit;
 
-  char *currentPath, *NextPath;
+  char *currentPath;
   
    // Go Parent directory, then Open RecursiveFolderCoper Folder
   
 	//struct stat file_info;
-	char Arr[512];
+	//char Arr[512];
 	//Shiro = fopen("Gay123.txt","r");
 	//Tut1 = fopen("Tut1.txt","w");
 
-  if (argn != "."){
+  if (filepath[0] != "."){
     currentPath =filepath;
-    sprintf(NextPath,"%s/%s/",currentPath,argn);
+    
 
   }
   else{
     currentPath=".";
-    NextPath=".";
+    
 
   }
    folder = opendir(currentPath);
@@ -57,32 +65,34 @@ void openandclosedir(char *argn,char* filepath)
 	//Checks folder/file Situation Open/NOTopen
 		if ( folder == NULL ){ // Shiro == NULL && Tut1 == NULL &&
 			//Printfail message
-		printf("Sorry, File/Folder has not been opened");
+		printf("Sorry, File/Folder has not been opened\n");
 	
 	}
 
 
 	else{
 		//print success message
-		printf("File opened successlly");
+		printf("Folder opened successlly\n");
 			
 			/*fscanf(Shiro,"%[^\0]s",Arr); proof of concpet learning
 			fprintf(Tut1,"%s\n Znullptr is gay ",Arr);*/
 
         		while((shit=readdir(folder))!=NULL){
+
         			if (strcmp(shit->d_name, ".") == 0) continue;   /* current dir */
     				if (strcmp(shit->d_name, "..") == 0) continue;  /* parent dir  */
     				
-    				if((isFile(shit->d_name) == 1) ){
+    				if((isFile(currentPath,shit->d_name) == 1) ){
      					printf("%s\t",shit->d_name);
      					printf("This is not a direcotry\n");
               
-     					copying_file(shit->d_name, NextPath);
+     					copying_file(shit->d_name,currentPath);
     				}
-    				if(isFile(shit->d_name) == 0 ){
+    				else{
             			printf("%s\t",shit->d_name);
+                  //printf("%s\t",shit->d_type);
      					printf("This is a direcotry\n") ;    
-                      copying_folders(shit->d_name,NextPath);    		
+                      copying_folders(shit->d_name,currentPath);    		
             						}
 
 
@@ -94,13 +104,23 @@ void openandclosedir(char *argn,char* filepath)
 
 }
 
-int isFile(const char *ff_name)
+int isFile(char *checkpath,const char *ff_name)
 {
+  char *rpath;
+    if (checkpath !="."){
 
-	char fileX[]= "./";
-	strcat(fileX,ff_name);
+    sprintf(rpath,"%s/%s",checkpath,ff_name);
 
-	DIR* directory =opendir(fileX);
+
+}
+else{ 
+
+  rpath = ff_name;
+}  
+	
+	
+
+	DIR* directory =opendir(rpath);
     
 
     if(directory != NULL)
@@ -120,13 +140,35 @@ int isFile(const char *ff_name)
 // Copies files existed in the Dir
 void copying_file(char *filecopied,char *argm)
 {
-	char ch;
-  char *filepath="../RecursiveFolderCopier/";;
+  char ch;
+  char filepath[]="../RecursiveFolderCopier/";
+  char tpath[260]="",rpath[260]="";
+	
+    if (argm !="."){
+
+    //sprintf(rpath,"%s/%s",argm,filecopied);
+    
+    strcat(rpath,argm);
+    strcat(rpath,"/");
+    strcat(rpath,filecopied);
+	
+    sprintf(tpath,"%s/%s/%s",filepath,FolderPathcorrector(argm),filecopied);
+
+}
+else{ 
+
+  strcat(rpath,filecopied);
+
+  sprintf(tpath,"%s/%s",filepath,filecopied);
+ 
+}  
+	
+
 
 	FILE *original,*target;
 
   long size;
-	original = fopen(filecopied,"r");
+	original = fopen(rpath,"r");
 
 	 if (original == NULL)
    {
@@ -134,7 +176,7 @@ void copying_file(char *filecopied,char *argm)
       exit(EXIT_FAILURE);
    }
    strcat(filepath,filecopied);
-   target = fopen(filepath, "w");
+   target = fopen(tpath, "w");
 
    	if(target == NULL)
     {
@@ -157,11 +199,29 @@ void copying_file(char *filecopied,char *argm)
 
 int copying_folders(char *foldercopied, char *path)
 {
-	char ch,*folderpath="../RecursiveFolderCopier/";
-  char *Hpath;
-  sprintf(Hpath,"%s/%s",folderpath, path);
-	DIR* folder1;
-	  folder1 = opendir(Hpath);
+char *folderpath="../RecursiveFolderCopier/";
+  char *tpath,t2path[512];
+  if (path !="."){
+
+   
+    sprintf(t2path,"%s/%s",path,foldercopied);
+    sprintf(tpath,"%s/%s/%s",folderpath,FolderPathcorrector(path),foldercopied);
+    mkdir(tpath);
+
+}
+else{ 
+
+  
+  sprintf(t2path,"./%s",foldercopied);
+  sprintf(tpath,"%s/%s",folderpath,foldercopied);
+  
+ mkdir(tpath);
+ printf("successfully created %s \n", foldercopied);
+}  
+  
+	
+  	DIR* folder1;
+	  folder1 = opendir(tpath);
 
 	
 	if(folder1 == NULL)
@@ -172,28 +232,40 @@ int copying_folders(char *foldercopied, char *path)
 
     else{
 
-    	
-        mkdir(foldercopied);
-    	 openandclosedir(foldercopied,path);
-
+      printf("%s\n", t2path);
+        //openandclosedir(foldercopied,t2path);
+    	openandclosedir(t2path);
 
     }
 
 
 }
+const char *  FolderPathcorrector(char *sher)
+{
+
+
+
+
+   char word2[512];
+
+   strcpy(word2,&sher[2]);
+
+   return word2;
+
+}
+
+
+
 
 /* UnWanted commentes
 lstat(shit->d_name,&file_info);
-
             		puts(dir_element->d_name);                       // show current filename
         		printf("file mode: %d\n", file_info.st_mode);*/
 
 
 
 /* Refrences 
-
 https://www.programmingsimplified.com/c-program-copy-file
-
 https://www.tutorialspoint.com/cprogramming/c_structures.htm
 https://www.programiz.com/c-programming/c-structures-pointers
 https://pubs.opengroup.org/onlinepubs/7908799/xsh/dirent.h.html
